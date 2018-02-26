6fcfa9a1a27da9f230ce546dcb91eb75da0ed23e
E Rasche <hxr@hx42.org>
2018-02-26 18:13:15 +0100

    add check for ftp file age
    fixes https://github.com/usegalaxy-eu/issues/issues/114

--- roles/hxr.simple-nagios/files/simple-nagios.sh
+++ roles/hxr.simple-nagios/files/simple-nagios.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+GALAXY_API_KEY=$(cat /etc/gx-api-creds.txt)
 
 expect_http() {
 	service=$1
@@ -62,6 +63,32 @@ EOF
 
 }
 
+expect_gx_ftp_age() {
+	service=$1
+	url=$2
+	t_start=$(date +%s.%N)
+	# Fetch the timestamp from the remote_files api
+	timestamp=$(curl --silent $url/api/remote_files?key=$GALAXY_API_KEY | jq '.[] | select(.path = "nagios") | .ctime' -r)
+	# Parse the date
+	created_at=$(date --date="$timestamp" "+%s")
+	now=$(date "+%s")
+	# Calculate its age.
+	file_age=$(echo "$now - $created_at" | bc)
+
+	t_end=$(date +%s.%N)
+	t_delta=$(echo "1000000 * ($t_end - $t_start)" | bc -l)
+	t_delta=$(echo $t_delta | sed 's/\..*//')
+
+	# Allow the file to be up to 300 seconds (5 minutes) old
+	if [[ $file_age -lt 300 ]]; then
+		status=0
+	else
+		status=1
+	fi
+
+	echo "eu.usegalaxy.services,service=$service request_time=0$t_delta,status=$status"
+}
+
 
 expect_http home_nossl http://usegalaxy.eu 301
 expect_http home https://usegalaxy.eu 200
@@ -85,3 +112,4 @@ expect_http influx http://influxdb.denbi.uni-freiburg.de:8086/ping 204
 expect_http sentry https://sentry.denbi.uni-freiburg.de/auth/login/sentry/ 200
 
 expect_ftps ftp_ssl ftp://ftp.usegalaxy.eu
+expect_gx_ftp_age ftp_age https://usegalaxy.eu
