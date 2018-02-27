b9332ed71b082d3bfbb72a64b802f18612a713f0
E Rasche <hxr@hx42.org>
2018-02-27 15:33:07 +0100

    Have telegraf launch some galaxy jobs

---
+++ roles/hxr.simple-nagios/files/simple-galaxy.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+from bioblend import galaxy
+import sys
+import time
+
+api_key = open('/etc/gx-api-creds.txt', 'r').read().strip()
+url = "https://usegalaxy.eu"
+
+handlers = [
+    "handler0",
+    "handler1",
+    "handler2",
+    "handler3",
+    "handler4",
+    "handler5",
+    "handler6",
+    "handler7",
+    "handler8",
+    "handler9",
+    "handler10",
+    "handler11",
+    "drmaa",
+    "condor"
+]
+
+history_name = "Nagios Run %s" % time.time()
+gi = galaxy.GalaxyInstance(url, api_key)
+history = gi.histories.create_history(name=history_name)
+history_id = history['id']
+print("History created %s" % history_id)
+
+jobs = []
+
+# Run all of the jobs.
+try:
+    for handler in handlers:
+        job = gi.tools.run_tool(history_id, 'echo_main_' + handler, {'echo': handler})
+        jobs.append({
+            'handler': handler,
+            'job': job,
+            'started': time.time()
+        })
+        print(job)
+except Exception as e:
+    print(e)
+
+    deleted = gi.histories.delete_history(history_id, purge=True)
+    print("History deleted %s" % deleted['id'])
+    sys.exit(1)
+
+try:
+    # Check all the jobs
+    for i in range(20):
+        states = []
+        for job in jobs:
+            state = gi.jobs.get_state(job['job']['jobs'][0]['id'])
+            states.append(state)
+            if state == 'ok' and 'finished' not in job:
+                job['finished'] = time.time()
+
+        if all([state in ('ok', 'error') for state in states]):
+            break
+
+        print(i, states)
+
+        # Otherwies we run until we hit end of range
+        time.sleep(2)
+
+    # Final states.
+    for job in jobs:
+        job['final_state'] = gi.jobs.get_state(job['job']['jobs'][0]['id'])
+        if 'finished' not in job:
+            job['finished'] = time.time()
+
+    # Now we're done, output something useful.
+    for job in jobs:
+        print("eu.usegalaxy.services,service=%s request_time=%s,status=%s" % (
+            job['handler'],
+            job['finished'] - job['started'],
+            0 if job['final_state'] == 'ok' else 1
+        ))
+
+except Exception as e:
+    print(e)
+
+    deleted = gi.histories.delete_history(history_id, purge=True)
+    print("History deleted %s" % deleted['id'])
