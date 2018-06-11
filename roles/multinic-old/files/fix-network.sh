74de133ad1e6ddf01116de4167891cfadb43e4d6
Helena Rasche <hxr@hx42.org>
2018-06-11 14:43:08 +0200

    unknown

--- roles/multinic-old/files/fix-network.sh
+++ roles/multinic-old/files/fix-network.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+ip route replace default     via 192.52.3.254       2> /dev/null || true
 ip route add 10.4.7.0/24     via 10.5.68.1 dev eth1 2> /dev/null || true
 #ip route add 132.230.68.0/24 via 10.5.68.1 dev eth1 2> /dev/null || true
