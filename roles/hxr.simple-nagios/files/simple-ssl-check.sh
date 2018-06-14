bec15fd50bbf8bd1824a11b246b2d2678cfe3fe5
Helena Rasche <hxr@hx42.org>
2018-06-14 17:19:58 +0200

    fix ranodm values for down

--- roles/hxr.simple-nagios/files/simple-ssl-check.sh
+++ roles/hxr.simple-nagios/files/simple-ssl-check.sh
@@ -2,7 +2,14 @@
 HOST=${1:-example.org}
 PORT=${2:-443}
 
-exprDate=$(echo | openssl s_client -servername $HOST -connect $HOST:$PORT 2>/dev/null | openssl x509 -noout -dates | grep notAfter | sed 's/notAfter=//g' | awk '{print $1,$2,$4}');
-unixExprDate=$(date -d "$exprDate" '+%s')
-secondsToExpr=$(echo "$unixExprDate - $(date '+%s')" | bc)
-echo "ssl.expiry,server=$HOST,port=$PORT value=$secondsToExpr"
+cert=$(echo | openssl s_client -servername $HOST -connect $HOST:$PORT 2>/dev/null)
+certExit=$?
+
+if [[ $certExit -eq 0 ]]; then
+	exprDate=$(echo "$cert" | openssl x509 -noout -dates | grep notAfter | sed 's/notAfter=//g' | awk '{print $1,$2,$4}');
+	unixExprDate=$(date -d "$exprDate" '+%s')
+	secondsToExpr=$(echo "$unixExprDate - $(date '+%s')" | bc)
+	echo "ssl.expiry,server=$HOST,port=$PORT value=$secondsToExpr"
+else
+	echo "ssl.expiry,server=$HOST,port=$PORT value=-1"
+fi
