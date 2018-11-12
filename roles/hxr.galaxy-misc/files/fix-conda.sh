418bf2f53b9abfb7da2e3a1c884fd93569e13f59
Helena Rasche <hxr@hx42.org>
2018-11-12 15:31:19 +0100

    some final cleanups

--- roles/hxr.galaxy-misc/files/fix-conda.sh
+++ roles/hxr.galaxy-misc/files/fix-conda.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+. /opt/galaxy/.bashrc
 for i in `find /usr/local/tools/_conda/envs/ -mindepth 1 -maxdepth 1 -type d`;
 do
     if [ ! -f $i/bin/conda ]; then
--- roles/hxr.galaxy-misc/files/fix-ftp.sh
+++ roles/hxr.galaxy-misc/files/fix-ftp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+. /opt/galaxy/.bashrc
 for username in $(psql -c 'COPY (select email from galaxy_user) TO STDOUT WITH CSV'); do
 	mkdir -p "/data/0/incoming/$username"
 done;
