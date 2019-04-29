f75c382fb2ab1c3c4bf5c377cddde2a10e865c5c
Helena Rasche <hxr@hx42.org>
2019-04-29 15:02:40 +0200

    swap to real db

--- roles/usegalaxy-eu.galactic-radio-telescope/templates/env.sh
+++ roles/usegalaxy-eu.galactic-radio-telescope/templates/env.sh
@@ -2,4 +2,4 @@
 export DJANGO_SETTINGS_MODULE=base.production
 export DJANGO_ALLOWED_HOSTS="telescope.galaxyproject.eu"
 export GRT_UPLOAD_DIR={{ grt_upload_dir }}
-export PGHOST="/var/run/postgresql" PGUSER="{{ grt_user }}" PGNAME="{{ grt_user }}" PGPORT="" PGPASSWORD=""
+export PGHOST="{{ grt_pghost }}" PGUSER="{{ grt_pguser }}" PGNAME="{{ grt_pgname }}" PGPORT="{{ grt_pgport }}" PGPASSWORD="{{ grt_pgpassword }}"
