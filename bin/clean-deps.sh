b5076e9200083a708167634256829859912473fb
Helena Rasche <hxr@hx42.org>
2019-05-27 10:59:26 +0200

    Add script to cleanup incorrect deps

---
+++ bin/clean-deps.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+error_code=0
+for repo__version in $(ansible-galaxy list | awk '{gsub(", ", "\t"); print $2"__"$3}'); do
+	vers="$(echo "$repo__version" | sed 's/.*__//g')"
+	repo="$(echo "$repo__version" | sed 's/__.*//g')"
+
+	# Ignore these, not proper ones.
+	if [[ "$vers" != "(unkonwn" ]]; then
+		results="$(grep "$repo" -A2 requirements.yaml)"
+		ec=$?
+
+		# Not under git's control
+		if (( ec == 0 )); then
+			expected_version=$(echo "$results" | grep version | sed 's/.*version: //g')
+
+			if [[ "$expected_version" != "master" ]]; then
+				if [[ "$vers" != "$expected_version" ]]; then
+					echo "Removing $repo: $vers != $expected_version";
+					ansible-galaxy remove "$repo"
+				fi
+			fi
+		fi
+	fi
+done
+
+exit $error_code
