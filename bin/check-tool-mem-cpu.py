de07ac05006e0ee47d9bebcc0358ce28e71b3fc9
Helena Rasche <hxr@hx42.org>
2019-07-15 16:48:26 +0200

    reformat

--- bin/check-tool-mem-cpu.py
+++ bin/check-tool-mem-cpu.py
@@ -3,19 +3,25 @@ import yaml
 import os
 import sys
 
-D = os.path.dirname(os.path.realpath(os.path.join(__file__, '..')))
+D = os.path.dirname(os.path.realpath(os.path.join(__file__, "..")))
 
-jcaas_conf = yaml.load(open(os.path.join(D, 'files/galaxy/dynamic_rules/usegalaxy/tool_destinations.yaml'), 'r'))
+jcaas_conf = yaml.load(
+    open(
+        os.path.join(D, "files/galaxy/dynamic_rules/usegalaxy/tool_destinations.yaml"),
+        "r",
+    )
+)
 jcaas_conf2 = {}
 for (k, v) in jcaas_conf.items():
     jcaas_conf2[k.lower()] = v
 
+
 def get_tool_id(tool_id):
-    if tool_id.count('/') == 0:
+    if tool_id.count("/") == 0:
         return tool_id
 
-    if tool_id.count('/') == 5:
-        (server, _, owner, repo, name, version) = tool_id.split('/')
+    if tool_id.count("/") == 5:
+        (server, _, owner, repo, name, version) = tool_id.split("/")
         return name
 
     return tool_id
@@ -25,17 +31,17 @@ max_mem = 0
 max_cpu = 0
 
 
-for v in sys.stdin.read().split('\n'):
+for v in sys.stdin.read().split("\n"):
     tool_id = get_tool_id(v).lower().strip()
 
     if tool_id in jcaas_conf2:
         tool_conf = jcaas_conf2[tool_id]
         print(tool_id, tool_conf)
 
-        if tool_conf.get('mem', 4) > max_mem:
-            max_mem = tool_conf.get('mem', 4)
+        if tool_conf.get("mem", 4) > max_mem:
+            max_mem = tool_conf.get("mem", 4)
 
-        if tool_conf.get('cores', 4) > max_cpu:
-            max_cpu = tool_conf.get('cores', 4)
+        if tool_conf.get("cores", 4) > max_cpu:
+            max_cpu = tool_conf.get("cores", 4)
 
-print('Maximums: memory=%s cpu=%s' % (max_mem, max_cpu))
+print("Maximums: memory=%s cpu=%s" % (max_mem, max_cpu))
--- bin/workflow-extract-tools.py
+++ bin/workflow-extract-tools.py
@@ -4,11 +4,11 @@ import sys
 
 
 def get_tool_id(tool_id):
-    if tool_id.count('/') == 0:
+    if tool_id.count("/") == 0:
         return tool_id
 
-    if tool_id.count('/') == 5:
-        (server, _, owner, repo, name, version) = tool_id.split('/')
+    if tool_id.count("/") == 5:
+        (server, _, owner, repo, name, version) = tool_id.split("/")
         return name
 
     return tool_id
@@ -16,19 +16,19 @@ def get_tool_id(tool_id):
 
 def tools_from_wf(data):
 
-    for k, v in data['steps'].items():
-        if v['tool_id'] is None:
+    for k, v in data["steps"].items():
+        if v["tool_id"] is None:
             continue
 
-        if 'subworkflow' in v:
-            yield from tools_from_wf(v['subworkflow'])
+        if "subworkflow" in v:
+            yield from tools_from_wf(v["subworkflow"])
         else:
-            yield get_tool_id(v['tool_id'])
+            yield get_tool_id(v["tool_id"])
 
 
 def obtain():
     for f in sys.argv[1:]:
-        with open(f, 'r') as handle:
+        with open(f, "r") as handle:
             data = json.load(handle)
         yield from tools_from_wf(data)
 
