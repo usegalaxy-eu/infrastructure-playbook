da44490779bb47a6c00c5ad171d9d3c02d54cbbf
Helena Rasche <hxr@hx42.org>
2019-09-06 14:20:39 +0200

    dont buffer

--- roles/usegalaxy-eu.gapars-galaxy/templates/run.sh
+++ roles/usegalaxy-eu.gapars-galaxy/templates/run.sh
@@ -2,4 +2,5 @@
 . {{ gapars_dir }}/venv/bin/activate
 cd {{ gapars_dir }}/code/
 export CONFIG_PATH="{{ gapars_dir }}/config/config.yaml"
+export PYTHONUNBUFFERED=1
 exec gunicorn --workers {{ gapars_workers | default("4") }} --bind {{ gapars_listen_url }} app:app
