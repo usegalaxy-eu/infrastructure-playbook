963c82007af77ef3b7fb0c672844a55f3c59b736
Helena Rasche <hxr@hx42.org>
2019-09-26 13:58:44 +0200

    fix django thing

--- roles/usegalaxy-eu.galactic-radio-telescope/templates/env.sh
+++ roles/usegalaxy-eu.galactic-radio-telescope/templates/env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export DJANGO_SETTINGS_MODULE=base.production
-export DJANGO_ALLOWED_HOSTS="telescope.galaxyproject.eu"
+export DJANGO_ALLOWED_HOSTS="{{ grt_allowed_hosts }}"
 export GRT_UPLOAD_DIR={{ grt_upload_dir }}
 export PGHOST="{{ grt_pghost }}" PGUSER="{{ grt_pguser }}" PGNAME="{{ grt_pgname }}" PGPORT="{{ grt_pgport }}" PGPASSWORD="{{ grt_pgpassword }}"
