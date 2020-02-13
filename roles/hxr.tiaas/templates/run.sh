bcaeff58103c92cf0b6a913149f96048ab16a8ed
Helena Rasche <hxr@hx42.org>
2020-02-13 16:54:43 +0100

    finally remove tiaas1!

--- roles/hxr.tiaas/templates/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-. {{ tiaas_dir }}/venv/bin/activate
-cd {{ tiaas_dir }}/code/
-export CONFIG_PATH="{{ tiaas_dir }}/config/config.yaml"
-exec gunicorn --workers {{ tiaas_workers | default("4") }} --bind {{ tiaas_listen_url }} app:app
