7848a47dbd49b76e574a23e1a7e5db3e0f84de7b
Gianmauro Cuccuru <gmauro@gmail.com>
2020-04-23 16:46:55 +0200

    update sorting hat

--- files/galaxy/dynamic_rules/usegalaxy/sorting_hat.py
+++ files/galaxy/dynamic_rules/usegalaxy/sorting_hat.py
@@ -147,11 +147,11 @@ def build_spec(tool_spec, dest_spec=SPECIFICATIONS, runner_hint=None):
     destination = runner_hint if runner_hint else tool_spec.get('runner')
 
     if destination not in dest_spec:
-        destination = DEFAULT_DESTINATION
+        if destination in JOINT_DESTINATIONS:
+            destination = sample(JOINT_DESTINATIONS[destination], 1)[0]
+        else:
+            destination = DEFAULT_DESTINATION
 
-    if destination == 'remote_condor_cluster_gpu_docker':
-        destination = sample(JOINT_DESTINATIONS['remote_condor_cluster_gpu_docker'], 1)[0]
-    
     env = dict(dest_spec.get(destination, {'env': {}})['env'])
     params = dict(dest_spec.get(destination, {'params': {}})['params'])
     tags = {dest_spec.get(destination).get('tags', None)}
