f8082488b6bf2a7f262cbe0197234b497acf7fbe
Gianmauro Cuccuru <gmauro@gmail.com>
2021-01-03 19:24:57 +0100

    check if an NFS mount point is stuck

--- roles/hxr.galaxy-misc/files/fix-ftp.sh
+++ roles/hxr.galaxy-misc/files/fix-ftp.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 . /opt/galaxy/.bashrc
-for username in $(psql -c 'COPY (select email from galaxy_user) TO STDOUT WITH CSV'); do
-	mkdir -p "/data/0/incoming/$username"
-done;
+
+stat_timeout=0.1 # seconds
+nfs_mp="/data/0"
+
+timeout -s kill $stat_timeout stat -t $nfs_mp > /dev/null
+if [[ ! $? == 137 ]];  then
+	for username in $(psql -c 'COPY (select email from galaxy_user) TO STDOUT WITH CSV'); do
+		mkdir -p "$nfs_mp/incoming/$username"
+	done;
+fi
