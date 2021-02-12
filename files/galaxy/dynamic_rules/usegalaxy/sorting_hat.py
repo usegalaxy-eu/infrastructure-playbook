e8f7847fda9e0ea1c8284a1558086b254a8668b5
Gianmauro Cuccuru <gmauro@gmail.com>
2021-02-12 16:59:51 +0100

    add special requirements for the 02.2021 Galaxy Training event

--- files/galaxy/dynamic_rules/usegalaxy/sorting_hat.py
+++ files/galaxy/dynamic_rules/usegalaxy/sorting_hat.py
@@ -356,6 +356,8 @@ def _finalize_tool_spec(tool_id, user_roles, tools_spec=TOOL_DESTINATIONS, memor
     # These we're running on a specific subset
     elif 'interactive_tool_' in tool_id:
         tool_spec['requirements'] = 'GalaxyDockerHack == True && GalaxyGroup == "compute"'
+        if tool_id == 'interactive_tool_rstudio':
+            tool_spec['requirements'] = 'GalaxyGroup == "interactive"'
 
     return tool_spec
 
