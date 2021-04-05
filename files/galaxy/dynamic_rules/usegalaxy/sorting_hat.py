103118db04e8019c428955ea0b1cb842bf256f77
Gianmauro Cuccuru <4559911+gmauro@users.noreply.github.com>
2021-04-05 12:03:03 +0200

    update requirements for docker destination

--- files/galaxy/dynamic_rules/usegalaxy/sorting_hat.py
+++ files/galaxy/dynamic_rules/usegalaxy/sorting_hat.py
@@ -359,6 +359,8 @@ def _finalize_tool_spec(tool_id, user_roles, tools_spec=TOOL_DESTINATIONS, memor
     # These we're running on a specific subset
     elif 'interactive_tool_' in tool_id:
         tool_spec['requirements'] = 'GalaxyDockerHack == True && GalaxyGroup == "compute"'
+    elif tool_id in ('msconvert', 'glassgo'):
+        tool_spec['requirements'] = 'GalaxyDockerHack == True && GalaxyGroup == "compute"'
         
     return tool_spec
 
