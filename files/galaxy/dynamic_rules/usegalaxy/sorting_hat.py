01c10d70981823f8b60456b85eb6699c9863f1d2
Gianmauro Cuccuru <4559911+gmauro@users.noreply.github.com>
2021-07-07 10:42:00 +0200

    filter for mothur* jobs

--- files/galaxy/dynamic_rules/usegalaxy/sorting_hat.py
+++ files/galaxy/dynamic_rules/usegalaxy/sorting_hat.py
@@ -363,6 +363,8 @@ def _finalize_tool_spec(tool_id, user_roles, tools_spec=TOOL_DESTINATIONS, memor
         tool_spec['requirements'] = 'GalaxyDockerHack == True && GalaxyGroup == "compute"'
     elif tool in ('msconvert', 'glassgo'):
         tool_spec['requirements'] = 'GalaxyDockerHack == True && GalaxyGroup == "compute"'
+    elif 'mothur' in tool:
+        tool_spec['requirements'] = 'GalaxyGroup == "compute_mothur"'
 
     return tool_spec
 
