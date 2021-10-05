ccd0b62fd5850f5331d863a04a8a45d4e5ee78b5
Gianmauro Cuccuru <4559911+gmauro@users.noreply.github.com>
2021-10-05 11:52:52 +0200

    switch to the new ftp server

--- roles/hxr.galaxy-misc/files/fix-conda.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-. /opt/galaxy/.bashrc
-for i in `find /usr/local/tools/_conda/envs/ -mindepth 1 -maxdepth 1 -type d`;
-do
-    if [ ! -f $i/bin/conda ]; then
-        /usr/local/tools/_conda/bin/conda ..checkenv bash $i
-    fi
-done
