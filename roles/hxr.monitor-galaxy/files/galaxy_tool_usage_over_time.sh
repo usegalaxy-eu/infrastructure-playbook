c77ef27f8c6f40088be942e1d468bd6bc4574b0a
Sanjay Srikakulam <sanjaysrikakulam@gmail.com>
2025-06-10 21:32:33 +0000

    Add galaxy tool usage over time telegraf script and tasks

---
+++ roles/hxr.monitor-galaxy/files/galaxy_tool_usage_over_time.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Description: This script will collect the usage of galaxy tools over time for the last two years and output it in a format suitable for ingestion into the influxdb.
+# Also the tool_id is cleaned up to remove the last part (such as version info) of the path and replace spaces with underscores.
+
+gxadmin query q "COPY (SELECT date_trunc('month', create_time)::date AS month, REPLACE(regexp_replace(tool_id, '/[^/]+$', ''), ' ', '_') AS tool_id, COUNT(*) AS
+usage_count FROM job  WHERE create_time >= date_trunc('month', NOW()) - INTERVAL '2 years' GROUP BY month, tool_id) TO STDOUT WITH (FORMAT CSV, HEADER FALSE, DELIMITER ',');" | awk -F, '{p
+rintf "galaxy_tool_usage_over_time,tool_id=%s,month=%s usage_count=%d\n", $2, $1, $3}'
