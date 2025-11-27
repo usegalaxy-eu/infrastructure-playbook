fc16410565766b7a6fdfa664cc831f7f2e4ecc38
gsaudade99 <gabri.saudade@gmail.com>
2025-11-27 12:23:50 +0100

    add crontab to the cleanup script

--- roles/usegalaxy-eu.galaxy-cleanup/templates/galaxy_cleanup_objectstores.sh
+++ roles/usegalaxy-eu.galaxy-cleanup/templates/galaxy_cleanup_objectstores.sh
@@ -5,5 +5,5 @@
 set -e
 
 {% for item in galaxy_pgcleanup_objectstores | default([]) %}
-{{ galaxy_venv_dir }}/bin/python {{ galaxy_server_dir }}/scripts/cleanup_datasets/pgcleanup.py -c {{ galaxy_config_file }} -o {{ item.days }} -l {{ galaxy_log_dir }} -w 128MB --object-store-id {{ item.objectstore_id }} purge_old_hdas 2>&1 | tee -a {{ galaxy_log_dir }}/cleanup_objectstore_datasets.log 1Code has comments. Press enter to view.
+{{ galaxy_venv_dir }}/bin/python {{ galaxy_server_dir }}/scripts/cleanup_datasets/pgcleanup.py -c {{ galaxy_config_file }} -o {{ item.days }} -l {{ galaxy_log_dir }} -w 128MB --object-store-id {{ item.objectstore_id }} purge_old_hdas 2>&1 | tee -a {{ galaxy_log_dir }}/cleanup_objectstore_datasets.log
 {% endfor %}
